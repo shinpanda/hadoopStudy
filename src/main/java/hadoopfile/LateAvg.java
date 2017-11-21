8e20792ccc30cdf80d0b2a39daf0857da9227efc
hduser <hduser@webserver>
2017-11-21 14:52:55 +0900

    11/21

--- src/main/java/hadoopfile/LateAvg.java
+++ src/main/java/hadoopfile/LateAvg.java
@@ -15,42 +15,60 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class LateAvg {
 
-	public static class TokenCounterMapper extends Mapper<Object, Text, Text, FloatWritable> { // 중첩 클래스(스테틱 중첩클래스)
+	public static class BehindTimeMapper extends Mapper<Object, Text, Text, FloatWritable> { // 중첩 클래스(스테틱 중첩클래스)
 		// 내용물만 바꿔 재사용
-		private Text word = new Text();
-		private final FloatWritable time = new FloatWritable(1);
+		private Text idWritable = new Text();
+		private FloatWritable timeWritable = new FloatWritable();
 
 		@Override
 		protected void map(Object key, Text value/* 라인을 읽어옴 */,
 				Mapper<Object, Text, Text, FloatWritable>.Context context) throws IOException, InterruptedException {
 			// 매핑을 위한 함수
 			String line = value.toString();
-			String[] split = line.split("\t");
-			word.set(split[0]);
-			time.set(Float.parseFloat(split[1]));
-			context.write(word, time);
+			String[] tokens = line.split("\t");
+			idWritable.set(tokens[0]);
+			timeWritable.set(Float.parseFloat(tokens[1]));
+			context.write(idWritable, timeWritable);
 
 		}
 	}
 
 	public static class TimeAvgReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> { // 집계된 결과
-
-		FloatWritable result = new FloatWritable();
+		private float avgTotal = 0;
+		private int countTotal = 0;
+		private FloatWritable avgWritable = new FloatWritable();
 
 		@Override
-		protected void reduce(Text key, Iterable<FloatWritable> values,
-				Reducer<Text, FloatWritable, Text, FloatWritable>.Context context)
+		protected void setup(Reducer<Text, FloatWritable, Text, FloatWritable>.Context context)
 				throws IOException, InterruptedException {
-			float sum = 0;
-			int count = 0;
-			for(FloatWritable val:values) {
-				sum += val.get();
-				count ++;
-			}
-			result.set(sum/count);
-			context.write(key, result);
+			// TODO Auto-generated method stub
+			super.setup(context);
 		}
 
+		@Override
+		protected void reduce(Text key, Iterable<FloatWritable> times,
+				Reducer<Text, FloatWritable, Text, FloatWritable>.Context context)
+				throws IOException, InterruptedException {
+			countTotal++;
+			float total = 0;
+			float avg = 0;
+			int count = 0;
+			for (FloatWritable time : times) {
+				total += time.get();
+				count++;
+			}
+			avg = total / count;
+			avgTotal += avg;
+			avgWritable.set(avg);
+			context.write(key, avgWritable);
+		}
+
+		@Override
+		protected void cleanup(Reducer<Text, FloatWritable, Text, FloatWritable>.Context context)
+				throws IOException, InterruptedException {
+			FloatWritable avgTotalWritable = new FloatWritable(avgTotal / countTotal);
+			context.write(new Text("total"), avgTotalWritable);
+		}
 	}
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
@@ -58,7 +76,8 @@ public class LateAvg {
 		Job job = Job.getInstance(conf, "Late Avg");
 
 		job.setJarByClass(LateAvg.class);
-		job.setMapperClass(TokenCounterMapper.class);
+		job.setMapperClass(BehindTimeMapper.class);
+		job.setCombinerClass(TimeAvgReducer.class);
 		job.setReducerClass(TimeAvgReducer.class);
 
 		job.setOutputKeyClass(Text.class);
